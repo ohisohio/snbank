1d5b6f05a0b84dd43cf4344b3356189c44b2197c
ohisohio <61963295+ohisohio@users.noreply.github.com>
2020-05-08 21:06:37 +0100

    Program structure

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 import random
+import os
+print("welcome to the banking system")
+options = input ("Do you want to login? Enter Yes or No>>> ")
+if options.upper() == "NO":
+    print("Thanks for you time")
+else:
+    print("Enter you username and password")
+    username = input("Username: ")
+    password = input("Password: ")
 
-file = open('staff.txt','r')
-for row in file:
-    field = row.split(",")
-    dbusername = (field[0])
-    dbpass = (field[1])
-  
+    content = open("staff.txt", "r")
+    for line in content:
+        contentdetails = line.split()
+        if username == contentdetails[0] and password == contentdetails[2]:
+            content.close()
+            options = input ("""if you want to create or check account enter 'A' 
+            if you want logout enter 'B'""")
+            if options.upper() == "A":
+                status = True
+                while status:
+                    accountOptions= input("""if you want to create account enter 'A', 
+                    if you want to check account details enter 'b'""")
+                    if accountOptions.upper() == "A":
+                        print("Enter the account details")
+                        accountEmail = input("AccountEmail: ")
+                        accountName = input("AccountName: ")
+                        accountType = input("AccountType: ")
+                        openingBalance = input("Opening: ")
+                        accountGeneration = random.randint(1234506789, 9876543210)
+                        accountNumber = str(accountGeneration)
 
-print("=========Welcome to SN BANK================")
+                        account = open("customer.txt", "a")
+                        account.write(accountEmail+ " "+accountName +" " +accountType+" " +accountNumber+" " +accountName+"\n")
+                        account.close()
+                    else:
+                        customerAccountNum = input("Account Number: ")
+                        customer = open("customer.txt", "r")
+                        for row in customer:
 
-print("=<< 1. Login         >>=")
-print("=<< 2. Close app     >>=")
+                            customerDetails = row.split()
+                            if customerDetails[3] == customerAccountNum:
+                                print(customerDetails)
 
-start = input('What would you like to do? 1 or 2: ')
-
-if start == '1':
-    print('PLEASE LOGIN')
-    username = str(input('Enter your staff username: '))
-    password = str(input('Enter your staff password: '))
-    if username == dbusername and password == dbpass:
-        print('Login Successful')
+                    user = input("Would you like to continue? enter yes or no ")
+                    if user.upper() == "NO":
+                        status = False
+                        print("successfully logged out")
+                    else:
+                        status = True
+            else:
+                print("you have successfully logged out")
+            break
     else:
-        print('unsuccessful')
-#with open('staff.txt', 'r') as reader:
-# Read & print the entire file
-    #print (reader.read())
+        print("session terminated due to invalid details")
+
+
+
+
+
+
+
+
+
+
+
 
 
